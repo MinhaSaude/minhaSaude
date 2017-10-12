f7a4b5600b86b43543a79823ca5bab8098e2a9b0
Gabrielr47 <gabrielr47@gmail.com>
2017-10-11 23:09:43 -0300

    menu auth and about page

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -9,24 +9,41 @@ import { SplashScreen } from '@ionic-native/splash-screen';
 export class MyApp {
   @ViewChild(Nav) nav: Nav;
   rootPage: any = 'HomePage';
-  fichaMedica: any = 'FichaMedicaPage';
-  historico: any = 'HistoricoPage';
-  medicos: any = 'MedicosPage';
-  cartao: any = 'CartaoPage';
-  sobre: any = 'SobrePage';
-  inicio: any = 'HomePage';
+  pages: Array<{ title: string, component: any, image: string }>;
+  isAuthenticated: boolean = false;
 
-  constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
+  constructor(
+    platform: Platform,
+    statusBar: StatusBar,
+    splashScreen: SplashScreen) {
     platform.ready().then(() => {
-      // Okay, so the platform is ready and our plugins are available.
-      // Here you can do any higher level native things you might need.
       statusBar.styleDefault();
       splashScreen.hide();
+      this.auth();
     });
   }
 
+  auth() {
+
+    if (this.isAuthenticated) {
+      this.pages = [
+        { title: 'Início', component: 'HomePage', image: './assets/icon/menu/inicio.png' },
+        { title: 'Ficha Médica', component: 'FichaMedicaPage', image: './assets/icon/menu/ficha-medica.png' },
+        { title: 'Histórico', component: 'HistoricoPage', image: './assets/icon/menu/historico.png' },
+        { title: 'Médicos', component: 'MedicosPage', image: './assets/icon/menu/medico.png' },
+        { title: 'Cartão', component: 'CartaoPage', image: './assets/icon/menu/cartao.png' },
+        { title: 'Sobre', component: 'SobrePage', image: './assets/icon/menu/sobre.png' }
+      ];
+    } else {
+      this.pages = [
+        { title: 'Início', component: 'HomePage', image: './assets/icon/menu/inicio.png' },
+        { title: 'Sobre', component: 'SobrePage', image: './assets/icon/menu/sobre.png' }
+      ];
+    }
+
+  }
   openPage(page) {
-    this.nav.setRoot(page);
+    this.nav.setRoot(page.component);
   }
 
 }
